ad8f88efc08d02e2561f9151e379947b0daba3e4
denis-vp <pop.denis.v@gmail.com>
2024-04-02 00:23:30 +0300

    Add fab

--- client/src/pages/Notes/Notes.tsx
+++ client/src/pages/Notes/Notes.tsx
@@ -1,27 +1,36 @@
-import { useNoteStore } from '../../state/noteStore';
-import NoteCard from '../../components/NoteCard';
-import Masonry from 'react-masonry-css';
-import styles from './Notes.module.css'
+import { useNoteStore } from "../../state/noteStore";
+import NoteCard from "../../components/NoteCard";
+import Masonry from "react-masonry-css";
+import styles from "./Notes.module.css";
+import Fab from "@mui/material/Fab/Fab";
+import AddIcon from "@mui/icons-material/Add";
 
 function Notes() {
-  const {notes} = useNoteStore();
+  const { notes } = useNoteStore();
 
-  return <>
-    <Masonry
-      breakpointCols={{default: 3, 1100: 2, 700: 1}}
-      className={styles.myMasonryGrid}
-      columnClassName={styles.myMasonryGridColumn}
-    >
-      {notes.map((note) => (
-        <div key={note.id}>
-          <NoteCard
-            note={note}
-          />
-        </div>
-      
-      ))}
-    </Masonry>
-  </>;
+  return (
+    <>
+      <Masonry
+        breakpointCols={{ default: 3, 1100: 2, 700: 1 }}
+        className={styles.myMasonryGrid}
+        columnClassName={styles.myMasonryGridColumn}
+      >
+        {notes.map((note) => (
+          <div key={note.id}>
+            <NoteCard note={note} />
+          </div>
+        ))}
+      </Masonry>
+
+      <Fab
+        color="primary"
+        aria-label="add"
+        sx={{ position: "fixed", right: 16, bottom: 16 }}
+      >
+        <AddIcon />
+      </Fab>
+    </>
+  );
 }
 
 export default Notes;
