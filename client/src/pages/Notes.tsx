b69fd737d35f9237379227bc9930e00867aa3cf5
denis-vp <pop.denis.v@gmail.com>
2024-04-02 11:59:03 +0300

    Replace react-masonry-css with mui masonry

--- client/src/pages/Notes.tsx
+++ client/src/pages/Notes.tsx
@@ -1,7 +1,6 @@
-import { useNoteStore } from "../../state/noteStore";
-import NoteCard from "../../components/NoteCard";
-import Masonry from "react-masonry-css";
-import styles from "./Notes.module.css";
+import { useNoteStore } from "../state/noteStore";
+import NoteCard from "../components/NoteCard";
+import Masonry from "@mui/lab/Masonry";
 import Fab from "@mui/material/Fab/Fab";
 import AddIcon from "@mui/icons-material/Add";
 import { useState } from "react";
@@ -12,16 +11,9 @@ function Notes() {
 
   return (
     <>
-      <Masonry
-        breakpointCols={{ default: 3, 1100: 2, 700: 1 }}
-        className={styles.myMasonryGrid}
-        columnClassName={styles.myMasonryGridColumn}
-      >
+      <Masonry columns={3} spacing={2} sx={{margin: 0, padding: 0}}>
         {notes.map((note) => (
-          <div
-            key={note.id}
-            onClick={() => setSelectedNoteId(note.id)}
-          >
+          <div key={note.id} onClick={() => setSelectedNoteId(note.id)}>
             <NoteCard note={note} selected={selectedNoteId === note.id} />
           </div>
         ))}
