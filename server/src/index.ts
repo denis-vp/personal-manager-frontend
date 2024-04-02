63018f3ef35c731c7e80f6e2899913ac43693251
denis-vp <pop.denis.v@gmail.com>
2024-04-02 11:36:47 +0300

    Update server

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,19 @@
-import fs from "fs";
+import express, { Express, Request, Response } from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app: Express = express();
+const port = process.env.PORT;
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("All good!");
+});
+
+app.listen(port, () => {
+  console.group();
+  console.log(`Server started at port ${port}`);
+  console.groupEnd();
+});
+
+export default app;
